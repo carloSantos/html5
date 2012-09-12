cb88a495e7cc0914dc0e6d7157ec4a9f72502b77
Ditmar <spyatorio@gmail.com>
2012-09-11 19:31:38 -0600

    enviamos

---
+++ galeria/galeria/Controllers/GaleriaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using galeria.Models;
+namespace galeria.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        //
+        // GET: /Galeria/
+
+        public ActionResult Index()
+        {
+            DataClasses1DataContext db =
+                new DataClasses1DataContext();
+            ViewBag.lista = db.imagenes.ToList();
+            return View();
+        }
+
+    }
+}
